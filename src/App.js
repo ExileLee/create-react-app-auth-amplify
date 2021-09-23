329da547e07b709c64b93b38119f151fc6ffdc49
ExileLee <exile1030@gmail.com>
2021-09-24 02:27:26 +0800

    add api fetch

--- src/App.js
+++ src/App.js
@@ -3,11 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 import Amplify from 'aws-amplify';
+import axios from "axios";
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
 
 class App extends Component {
+
   render() {
+    const apiBaseUrl = "https://api.boyilab.com";
+
+    const api = axios.create({
+        baseURL: apiBaseUrl
+    });
+
+    api.get("artists/the-1975")
+      .then(x => {
+        console.log(x.data);
+      })
+
     return (
       <div className="App">
         <AmplifySignOut />
